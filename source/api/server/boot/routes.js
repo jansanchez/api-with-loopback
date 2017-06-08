96b244619ebaa4b26a9462600c0a2e0b1622bbe0
Jan Sanchez <joejansanchez@gmail.com>
2017-06-08 17:20:29 -0500

    feat server: agrego una custom route

---
+++ source/api/server/boot/routes.js
@@ -0,0 +1,55 @@
+var rp = require('request-promise');
+var domain = 'http://192.168.2.6:3000/api/';
+var json = {};
+
+module.exports = function(app) {
+  app.get('/api/synchronize', function(req, res) {
+    var options = {
+      uri: domain + 'companies',
+      headers: {
+        'User-Agent': 'Request-Promise'
+      },
+      json: true
+    };
+
+    var optionsForAreas = options;
+    optionsForAreas.uri = domain + 'areas';
+
+    var optionsForLocations = options;
+    optionsForLocations.uri = domain + 'locations';
+
+    json.status = 'SUCCESS';
+    json.code = 200;
+    json.message = 'synchronize executed successful';
+    json.data = {};
+
+    rp(options)
+      .then(function (companies) {
+        json.data.companies = companies;
+      })
+      .catch(function (err) {
+        json.error = err;
+      });
+
+    rp(optionsForAreas)
+      .then(function (areas) {
+        json.data.areas = areas;
+      })
+      .catch(function (err) {
+        json.error = err;
+      });
+
+    rp(optionsForLocations)
+      .then(function (locations) {
+        json.data.locations = locations;
+      })
+      .catch(function (err) {
+        json.error = err;
+      });
+
+    setTimeout( function(){
+      res.send(json);
+    }, 500);
+
+  });
+}
